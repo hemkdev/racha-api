6a145f235f53d32f37e28979dcdaf7a04b63d3e0
hemkdev <lucas.hemkemeier007@gmail.com>
2026-08-30 22:58:52 -0300

    chore: omit migration on purpose to verify ci blocks merge

--- core/models.py
+++ core/models.py
@@ -1,7 +1,8 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 # Create your models here.
 
 
 class User(AbstractUser):
-    pass
+    phone = models.CharField(max_length=20, blank=True, null=True)
