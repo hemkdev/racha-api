c86c163ea19f015c602995b6937b2deb42aa1c78
hemkdev <lucas.hemkemeier007@gmail.com>
2026-09-11 13:34:50 -0300

    feat: register court and user in django admin

--- core/admin.py
+++ core/admin.py
@@ -1,1 +1,8 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from core.models import Court, User
+
 # Register your models here.
+admin.site.register(User, UserAdmin)
+admin.site.register(Court)
