be60b6904a9615a9fac31d1bbcdd9c36012583a9
hemkdev <lucas.hemkemeier007@gmail.com>
2026-09-17 21:48:48 -0300

    chore: drop django scaffold leftovers

--- core/admin.py
+++ core/admin.py
@@ -3,7 +3,6 @@ from django.contrib.auth.admin import UserAdmin
 
 from core.models import Booking, Court, User
 
-# Register your models here.
 admin.site.register(User, UserAdmin)
 admin.site.register(Court)
 admin.site.register(Booking)
